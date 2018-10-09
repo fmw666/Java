251a37251949ef6def6bcc21ec0da186f6e3a807
Fan MaoWei <34910514+fmw666@users.noreply.github.com>
2018-10-09 19:15:08 +0800

    匿名内部类--Java源程序

---
+++ Inner Class/AnonymousInnerClass.java
@@ -0,0 +1,50 @@
+public class AnonymousInnerClass{
+    public static void main(String []args){
+        Person p=new Person();
+        //Animal dog=new Dog();
+        //p.feed(dog);
+
+        new IAnimal(){
+        	private String name="aa";
+        	//static{
+        	//	name="哈哈";
+        	//}
+        	public void eat(){
+				System.out.println("啃骨头");
+			}
+			public void show(){
+				System.out.println(name);
+			}
+		}.show();
+
+        //p.feed(dog);
+        //dog.show(); 
+        //p.feed(dog);
+
+        p.feed(new IAnimal(){
+			public void eat(){
+				System.out.println("吃鱼肉");
+			}
+        });
+    }
+}
+
+class Person{
+	public void feed(IAnimal animal){
+		animal.eat();
+	}
+}
+
+/*abstract class Animal{
+	public abstract void eat();
+}*/
+
+interface IAnimal{
+	public abstract void eat();
+}
+
+/*class Dog extends Animal{
+	public void eat(){
+		System.out.println("啃骨头");
+	}
+}*/
