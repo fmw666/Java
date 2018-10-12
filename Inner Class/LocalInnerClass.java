8f2d450cd6bff9af2400161f9cfa656afa64b32d
Fan MaoWei <34910514+fmw666@users.noreply.github.com>
2018-10-12 20:18:15 +0800

    局部内部类--Java源程序

--- Inner Class/LocalInnerClass.java
+++ Inner Class/LocalInnerClass.java
@@ -10,22 +10,22 @@ class Outer3{
     private int num1=10;
     private static int num2=20;
     public void showOuter(){
-		    final int num4=50;
-		    //局部内部类不能加访问修饰符
-		    class Inner3{
-			      private int num3=30;
-			      private int num1=20;
-			      public void showInner(){
-				        System.out.println(num3);
-				        System.out.println(num1);
-				        System.out.println(Outer3.this.num1);
-				        System.out.println(Outer3.num2);
-				        System.out.println(num4);
-				        //局部内部类只能访问声明其方法中的常量 
-			  }	
-		}
-		Inner3 inner=new Inner3();
-		inner.showInner();
-	  }
+        final int num4=50;
+        //局部内部类不能加访问修饰符
+        class Inner3{
+            private int num3=30;
+            private int num1=20;
+            public void showInner(){
+                System.out.println(num3);
+                System.out.println(num1);
+                System.out.println(Outer3.this.num1);
+                System.out.println(Outer3.num2);
+                System.out.println(num4);
+                //局部内部类只能访问声明其方法中的常量 
+            }	
+	}
+	Inner3 inner=new Inner3();
+        inner.showInner();
+    }
 }
         
